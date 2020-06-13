59a90c6c8e6d78be3e880465198bc92aacd7b054
SwathyMittra <mittraswathy311@gmail.com>
2020-06-13 12:49:46 +0530

    Swathy - First project commit

---
+++ e2e/conf.js
@@ -0,0 +1,4 @@
+export const config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['todo-spec.js']
+  };
---
+++ tests/calculator.js
@@ -0,0 +1,21 @@
+describe('Calculator test', function()
+{
+    it ('addition of numbers', function()
+        {
+            browser.get('http://juliemr.github.io/protractor-demo/');
+
+            element(by.model('first')).sendKeys('2');
+            element(by.model('second')).sendKeys('2');
+
+            //element(by.css('[ng-click="doAddition()"]')).click();
+            element(by.xpath("//button[@ng-click='doAddition()']")).click();
+            browser.sleep(3000)
+            let result = element(by.cssContainingText('.ng-binding','4'))
+            
+            expect(result.getText()).toEqual('4');
+            
+
+            browser.sleep(3000)
+        }
+    )
+})
